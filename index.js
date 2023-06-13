2d029889b76b2302954eec7a3c2317f4e4db364d
sidee <said.issack@student.moringaschool.com>
2023-06-13 13:54:09 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
- // Write your code here!
-const ternaryCheckCity = (city) => {
+function scuberGreetingForFeet(feet){
+  // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!";
+  } else if (feet > 400 && feet <= 2000) {
+    return "That'll be $30.";
+  } else if (1999 <feet && feet< 2500) {
+    return "I will gladly take your thirty bucks.";
+  } 
+  else {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city){
+  // Write your code here!
   if (city === "NYC") {
     return "Ok, sounds good.";
   } else {
     return "No go.";
   }
-
-function ternaryCheckCity(){
-  // Write your code here!
 }
-};
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+}
